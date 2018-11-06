4437e928e11f20247a0ee5104208474fbb9cf590
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-11-06 15:32:56 -0800

    Start developing the frame.

---
+++ src/components/Board.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Square from "./Square";
+
+class Board extends Component {
+    drawSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = "Next player: X";
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.drawSquare(0)}
+                    {this.drawSquare(1)}
+                    {this.drawSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.drawSquare(0)}
+                    {this.drawSquare(1)}
+                    {this.drawSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.drawSquare(3)}
+                    {this.drawSquare(4)}
+                    {this.drawSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.drawSquare(6)}
+                    {this.drawSquare(7)}
+                    {this.drawSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/Game"
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
