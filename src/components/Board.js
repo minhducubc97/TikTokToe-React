063a2b2c9e20234629264d22d30d4f8b90afb037
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-11-17 20:54:12 -0800

    Working game with basic functionalities.

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,12 +5,20 @@ class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
-        }
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
     }
 
     render() {
-        const status = "Next player: X";
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        }
+        else {
+            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        }
 
         return (
             <div>
@@ -39,11 +47,39 @@ class Board extends Component {
     }
 
     handleClick(i) {
+        if (this.calculateWinner(this.state.squares) || (this.state.squares[i])) {
+            return;
+        }
         // create the copy of the array
         const copiedSquares = this.state.squares.slice();
-        copiedSquares[i] = 'X';
+        copiedSquares[i] = this.state.xIsNext ? 'X' : 'O';
         // trigger setState with the copied array
-        this.setState({ squares: copiedSquares });
+        this.setState
+            ({
+                squares: copiedSquares,
+                xIsNext: !this.state.xIsNext,
+            });
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2], // first row
+            [3, 4, 5], // second row
+            [6, 7, 8], // third row
+            [0, 3, 6], // first column
+            [1, 4, 7], // second column
+            [2, 5, 8], // third column
+            [0, 4, 8], // first diagonal
+            [2, 4, 6]  // second diagonal
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && (squares[a] === squares[b]) && (squares[a] === squares[c])) {
+                return squares[a];
+            }
+        }
+        return null;
     }
 }
 
